78512aab24f516a12d3692a77d3a48863ab65dd2
BenederBuali <neviobeneder79@gmail.com>
2026-03-30 13:28:49 +0100

    Change default port from 5000 to 10000

--- app.py
+++ app.py
@@ -1067,5 +1067,5 @@ def sicherstelle_thread():
 
 if __name__ == "__main__":
     starte_hintergrund_thread()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
